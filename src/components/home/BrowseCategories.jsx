0e700b8f9c962c8c81f6f0595747e123f80d5028
mahnaf <mahnaf125@gmail.com>
2025-05-31 15:44:56 +0600

    feat: update browse-categories section

--- src/components/home/BrowseCategories.jsx
+++ src/components/home/BrowseCategories.jsx
@@ -1,40 +1,15 @@
+import { useEffect, useState } from "react";
 import CategoryCard from "../shared/CategoryCard";
+import { Link } from "react-router";
 
 const BrowseCategories = () => {
-	const categoriesData = [
-		{
-			emoji: "💡",
-			name: "Web & Software",
-		},
-		{
-			emoji: "📊",
-			name: "Management",
-		},
-		{
-			emoji: "📞",
-			name: "Customer Help",
-		},
-		{
-			emoji: "👨🏻‍💼",
-			name: "Human Resource",
-		},
-		{
-			emoji: "🏦",
-			name: "Finance",
-		},
-		{
-			emoji: "📢",
-			name: "Marketing",
-		},
-		{
-			emoji: "📝",
-			name: "Content Writing",
-		},
-		{
-			emoji: "🔮",
-			name: "Editing",
-		},
-	];
+	const [categories, setCategories] = useState();
+	useEffect(() => {
+		fetch("http://localhost:5100/categories/hot")
+			.then((res) => res.json())
+			.then((data) => setCategories(data))
+			.catch((error) => console.log(error));
+	}, []);
 	return (
 		<section className="max-w-8xl mx-auto mt-24">
 			<h2 className="text-4xl font-bold text-center mb-2">Browse Categories</h2>
@@ -42,15 +17,26 @@ const BrowseCategories = () => {
 				Find the job that's perfect for you, categorized beautifully for you. Over 15+
 				categories.
 			</p>
-			<div className="mt-12 grid grid-cols-4 gap-6">
-				{categoriesData.map((category) => (
+			<div className="mt-12 mb-8 grid grid-cols-5 gap-5">
+				{categories?.map((category) => (
 					<CategoryCard
-						key={category.name}
-						emoji={category.emoji}
+						key={category._id}
+						icon={category.icon}
+						description={category.description}
 					>
 						{category.name}
 					</CategoryCard>
 				))}
+			</div>
+			<div className="text-center">
+				<Link to="/categories">
+					<button
+						type="submit"
+						className="w-40 py-2 text-2xl font-medium bg-prime text-light hover:bg-blue-500 rounded-xl cursor-pointer"
+					>
+						View All
+					</button>
+				</Link>
 			</div>
 		</section>
 	);
--- src/components/shared/CategoryCard.jsx
+++ src/components/shared/CategoryCard.jsx
@@ -1,13 +1,13 @@
-const CategoryCard = ({ emoji = "", children }) => {
+const CategoryCard = ({ children = "", icon = "", description = "" }) => {
 	return (
 		<div className="bg-gradient-to-br from-neutral-100 to-prime/15 p-6 border border-neutral-300 rounded-2xl flex items-center gap-x-4 hover:to-prime/25 hover:border-prime/40 transition-colors">
 			<div
 				role="img"
 				className="text-3xl"
 			>
-				{emoji}
+				{icon}
 			</div>
-			<h5 className="text-xl font-bold">{children}</h5>
+			<h5 className="text-xl font-semibold">{children}</h5>
 		</div>
 	);
 };
